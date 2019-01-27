dd54754881b6526ebd6469fbbeb508700d9d4ab9
Yuan CHEN <constantchen525@gmail.com>
2019-01-26 21:24:59 -0500

    part3@a1: initial

---
+++ Proj@1/Part3/RabbitMqClient.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class RabbitMqClient implements AutoCloseable {
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+
+    public RabbitMqClient(String ipaddress) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(ipaddress);
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+    public String send(String message) throws IOException, InterruptedException {
+        final String corrId = UUID.randomUUID().toString();
+
+        String replyQueueName = channel.queueDeclare().getQueue();
+        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder().correlationId(corrId).replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response.offer(new String(delivery.getBody(), "UTF-8"));
+            }
+        }, consumerTag -> {
+        });
+
+        String result = response.take();
+        channel.basicCancel(ctag);
+        return result;
+    }
+
+    public void close() throws IOException {
+        connection.close();
+    }
+
+    public static void main(String[] args) {
+        try(RabbitMqClient client = new RabbitMqClient(args[0])) {
+            int size = 1;
+            byte[] data = {0x01};
+            for (int i = 0; i< size; i ++) {
+                String stringData = new String(data);
+                System.out.println(" [x] Sending Data");
+                String response = client.send(stringData);
+                System.out.println(" [.] Got '" + response + "'");
+            }
+        } catch (IOException | TimeoutException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Proj@1/Part3/RabbitMqServer.java
@@ -0,0 +1,60 @@
+import com.rabbitmq.client.*;
+import java.lang.RuntimeException;
+
+public class RabbitMqServer {
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(args[0]); // Specifiy the address of the Broker
+
+        try(Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+            channel.queuePurge(RPC_QUEUE_NAME);
+
+            channel.basicQos(1);
+
+            System.out.println("[x] Awaiting RPC requests");
+
+            Object monitor = new Object();
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                    .Builder()
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+
+                String response = "";
+                int count = 0;
+
+                try {
+                    String message = new String(delivery.getBody(), "UTF-8");
+                    int n = Integer.parseInt(message);
+
+                    count += n;
+                } catch (RuntimeException e) {
+                    System.out.println("[.] " + e.toString());
+                } finally {
+                    response = "Received the " + Integer.toString(count) + "th Ack";
+                    channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+
+                    synchronized (monitor) {
+                        monitor.notify();
+                    }
+                }
+            };
+
+            channel.basicConsume(RPC_QUEUE_NAME, false, deliverCallback, (consumerTag -> {}));
+            // Wait and be prepared to consume the message from RPC client.
+            while (true) {
+                synchronized (monitor) {
+                    try {
+                        monitor.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
