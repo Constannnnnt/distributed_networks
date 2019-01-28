1f40979cbfa5bfb5f1938612b7d5648418094e04
Yuan CHEN <constantchen525@gmail.com>
2019-01-28 16:35:03 -0500

    part1@a1: update

--- Proj@1/Part1/tcpServer.java
+++ Proj@1/Part1/tcpServer.java
@@ -23,10 +23,9 @@ public class tcpServer {
         Socket client = this.server.accept();
         String clientAddress = client.getInetAddress().getHostAddress();
         System.out.println("\r\nNew connection from " + clientAddress);
+        BufferedInputStream in = new BufferedInputStream(client.getInputStream());
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
         while (true) {
-            BufferedInputStream in = new BufferedInputStream(client.getInputStream());
-            DataOutputStream out = new DataOutputStream(client.getOutputStream());
-
             int data = -1;
             if ((data = in.read()) != -1) {
                 System.out.println("\r\nMessage from " + clientAddress + ": " + data);
