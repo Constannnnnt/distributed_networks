904b4ff6a001eb7cb5257866306ff4b5f297c5b6
Yuan CHEN <constantchen525@gmail.com>
2019-01-28 22:00:43 -0500

    part2@a1: update

--- Proj@1/Part2/simplegrpc/src/main/java/com/simple/grpc/GRpcServiceImpl.java
+++ Proj@1/Part2/simplegrpc/src/main/java/com/simple/grpc/GRpcServiceImpl.java
@@ -9,7 +9,7 @@ public class GRpcServiceImpl extends GRpcServiceGrpc.GRpcServiceImplBase {
     public void sendData(DataRequest request,
             StreamObserver<DataResponse> responseObserver) {
         // HelloRequest has toString auto-generated.
-        System.out.println(request);
+        // System.out.println(request);
 
         // You must use a builder to construct a new Protobuffer object
         DataResponse response = DataResponse.newBuilder()
