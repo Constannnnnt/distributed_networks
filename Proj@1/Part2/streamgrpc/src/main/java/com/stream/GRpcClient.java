bd3e2603ba5148ab00f901e1e578543230552976
Yuan CHEN <constantchen525@gmail.com>
2019-01-29 12:29:03 -0500

    part2@a1: update

--- Proj@1/Part2/streamgrpc/src/main/java/com/stream/GRpcClient.java
+++ Proj@1/Part2/streamgrpc/src/main/java/com/stream/GRpcClient.java
@@ -57,10 +57,14 @@ public class GRpcClient {
 
         final CountDownLatch latch = new CountDownLatch(1);
 
+        float[] time = new float[100];
+        float totaltime = 0;
+        int size = 10;
         StreamObserver<DataResponse> responseObserver = new StreamObserver<DataResponse>() {
             @Override
             public void onNext(DataResponse response) {
                 System.out.println(response.getAck());
+                System.out.println(response.getElapsed());
             }
 
             public void onError(Throwable t) {
@@ -75,20 +79,22 @@ public class GRpcClient {
             }
         };
 
-
         StreamObserver<DataRequest> requestObserver = stub.sendData(responseObserver);
+
         try {
-            for (int i = 0; i < 10; i++) {
+            for (int k = 0; k < size; k++) {
+                System.out.println(" [x] Sending " + Integer.toString(k+1) + "th Data");
+                long start = System.nanoTime();
                 ByteString byteData = null;
-                byteData = ByteString.copyFrom(data, i * 1024 * 1024, (i + 1) * 1024 * 1024);
+                byteData = ByteString.copyFrom(data);
                 DataRequest request = DataRequest.newBuilder().setData(byteData).build();
                 requestObserver.onNext(request);
-            }
 
-            if (latch.getCount() == 0) {
-                // RPC completed or errored before we finished sending.
-                // Sending further requests won't error, but they will just be thrown away.
-                return;
+                if (latch.getCount() == 0) {
+                    // RPC completed or errored before we finished sending.
+                    // Sending further requests won't error, but they will just be thrown away.
+                    return;
+                }
             }
         } catch (RuntimeException r) {
             requestObserver.onError(r);
@@ -97,6 +103,21 @@ public class GRpcClient {
 
         requestObserver.onCompleted();
 
+        try {
+            latch.await(10, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            // Receiving happens asynchronously
+
+            // long end = System.nanoTime();
+            // float duration = end - start;
+            // time[k] = duration / 1000000000;
+            // totaltime += duration / 1000000000;
+        // System.out.println("Average Time: " + (totaltime / 100) + "seconds");
+        // float[] sortedTime = doInsertionSort(time);
+        // System.out.println("10th Percentile: " + sortedTime[9] + "seconds");
+        // System.out.println("90th Percentile: " + sortedTime[89] + "seconds");
 
     }
 
@@ -106,22 +127,7 @@ public class GRpcClient {
         for (int i = 0; i < 10 * 1024 * 1024; i++) {
             data[i] = 0x01;
         }
-        float[] time = new float[100];
-        float totaltime = 0;
 
-        for (int i = 0; i < 100; i++) {
-            System.out.println(" [x] Sending " + Integer.toString(i) + "Data");
-            long start = System.nanoTime();
-            client.sendData(data);
-            long end = System.nanoTime();
-            float duration = end - start;
-            time[i] = duration / 1000000000;
-            totaltime += duration / 1000000000;
-        }
-
-        System.out.println("Average Time: " + (totaltime / 100) + "seconds");
-        float[] sortedTime = doInsertionSort(time);
-        System.out.println("10th Percentile: " + sortedTime[9] + "seconds");
-        System.out.println("90th Percentile: " + sortedTime[89] + "seconds");
+        client.sendData(data);
     }
 }
--- Proj@1/Part2/streamgrpc/src/main/java/com/stream/GRpcServiceImpl.java
+++ Proj@1/Part2/streamgrpc/src/main/java/com/stream/GRpcServiceImpl.java
@@ -3,12 +3,14 @@ package com.stream.grpc;
 import io.grpc.stub.StreamObserver;
 import com.stream.grpc.GRpcServiceOuterClass.DataRequest;
 import com.stream.grpc.GRpcServiceOuterClass.DataResponse;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 public class GRpcServiceImpl extends GRpcServiceGrpc.GRpcServiceImplBase {
     @Override
     public StreamObserver<DataRequest> sendData(final StreamObserver<DataResponse> responseObserver) {
         return new StreamObserver<DataRequest>() {
             int result = 0;
+            long startTime = System.nanoTime();
 
             @Override
             public void onNext(DataRequest request) {
@@ -21,8 +23,9 @@ public class GRpcServiceImpl extends GRpcServiceGrpc.GRpcServiceImplBase {
 
             @Override
             public void onCompleted() {
-                String ret = "Acks!";
-                responseObserver.onNext(DataResponse.newBuilder().setAck(ret).build());
+                long seconds = NANOSECONDS.toSeconds(System.nanoTime() - startTime);
+                String ret = "Received " + Integer.toString(result) + "chunks of 10MB data, Acks!";
+                responseObserver.onNext(DataResponse.newBuilder().setAck(ret).setElapsed((int) seconds).build());
                 responseObserver.onCompleted();
             }
         };
